2bf5e21606c315713782fd363c06eec1f5069fad
YongHwi <yonghwinam@smtown.com>
2017-03-28 17:51:39 +0900

    dfdfdf

--- YHHello/Example/Tests/Tests.m
+++ YHHello/Example/Tests/Tests.m
@@ -24,7 +24,7 @@
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // Put teardown code here. This method is called after the invocation of each test  in the class.
     [super tearDown];
 }
 
